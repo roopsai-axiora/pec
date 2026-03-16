7ae6b58a94ba7cd97e4949cb151588db2c1235ee
roopsai <roopsai@outlook.in>
2026-03-16 10:40:19 +0530

    feat: initial Spring Boot Scaffold #1

---
+++ src/main/java/com/axiora/pec/PecApplication.java
@@ -0,0 +1,13 @@
+package com.axiora.pec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecApplication.class, args);
+	}
+
+}
